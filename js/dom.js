526493ccd9108555ca32dc60b36cc8f712d85cda
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-15 20:57:45 +0600

    Added 3 way to use onclick for change background color

--- js/dom.js
+++ js/dom.js
@@ -25,3 +25,26 @@
     </ul>
     `
     main.appendChild(section);
+
+
+    // Onclick Action 1
+    function makeRed(){
+        document.body.style.backgroundColor='red';
+    }
+
+// Onclick Action 2
+const blueButton= document.getElementById('makeBlue');
+blueButton.onclick=blueColor;  //onclick in html called by functionName()/but this time only functionName
+function blueColor() {
+    document.body.style.backgroundColor='blue';
+}
+
+
+// Onclick Action 3
+const purpleBtn=document.getElementById('purpleButton');
+console.log(purpleBtn);
+purpleBtn.onclick=function pupleMe(){
+    document.body.style.backgroundColor='purple';
+}
+
+
