f27e07453539cff1b0c01b5f404ce9bc1def059a
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-16 00:38:37 +0600

    Added text into Prent div using EventListener

--- js/weuse.js
+++ js/weuse.js
@@ -2,7 +2,17 @@ function mostUsed(){
     document.body.style.backgroundColor='grey'
 }
 
+// Option 1
+function handler(){
+    const handlerStatusChange=document.getElementById('paraToChange');
+    handlerStatusChange.innerText='Status Changed by handle button'
+}
 
 
+// Option 2
+document.getElementById('event-listener').addEventListener('click', function(){
+    const handlerStatus=document.getElementById('paraToChange');
+    handlerStatus.innerText='this is changed by second button'
+})
 
 
