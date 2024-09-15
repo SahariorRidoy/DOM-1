bdc778f0340f98b449742fc48c186b08ffaa1d8b
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-15 16:16:45 +0600

    appenChild example

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,27 @@
 
 
 
-    document.getElementById('list').style.backgroundColor='red';
+    // document.getElementById('list').style.backgroundColor='red';
 
 
-    
-    const list =document.querySelectorAll('#list li');
-    for(const lis of list){
-        console.log(lis.innerText);   
-    }
+
+    // const list =document.querySelectorAll('#list li');
+    // for(const lis of list){
+    //     console.log(lis.innerText);   
+    // }
+
+
+
+    const main = document.getElementById('main-container');
+    const section = document.createElement('section');
+
+    section.innerHTML=`
+    <h1>This is dynamic added Section Title</h1>
+    <ul>
+    <li>Items</li>
+    <li>Items</li>
+    <li>Items</li>
+    <li>Items</li>
+    </ul>
+    `
+    main.appendChild(section);
